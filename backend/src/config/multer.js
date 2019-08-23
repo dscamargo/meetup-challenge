65edd798269bf68c0b99bb1575220933f3f6eebb
Douglas S. Camargo <douglas_simon@hotmail.com>
2019-08-23 15:46:23 -0300

    alterado caminho destino multer

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,16 +1,16 @@
-import multer from "multer";
-import crypto from "crypto";
-import { extname, resolve } from "path";
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
 
 export default {
   storage: multer.diskStorage({
-    destination: resolve(__dirname, "..", "..", "tmp", "uploads"),
+    destination: resolve(__dirname, '..', '..', 'public', 'uploads'),
     filename: (req, file, callback) => {
       crypto.randomBytes(16, (err, res) => {
         if (err) return callback(err);
 
-        return callback(null, res.toString("hex") + extname(file.originalname));
+        return callback(null, res.toString('hex') + extname(file.originalname));
       });
-    }
-  })
+    },
+  }),
 };
